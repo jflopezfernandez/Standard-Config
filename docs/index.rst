0fd212a30cdfb8cd7cef46cc0a0bfc701ed46fa5
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-05-27 19:45:30 -0400

    Experimenting with perl and reStructuredText

--- docs/index.rst
+++ docs/index.rst
@@ -3,14 +3,40 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
+====
+StdConf
+====
 Welcome to StdConf's documentation!
-===================================
+------------------------
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    introduction
+
+The following is a sample grammar.
+
+.. describe:: productionlist
+
+   This directive is used to enclose a group of productions. Each production is
+   given on a single line and consists of a name, separated by a colon from the
+   following definition. If the definition spans multiple lines, each
+   continuation line must begin with a colon placed at the same column in the 
+   first line.
+
+   Blank lines are not allowed within ``productionlist`` directive arguments.
+
+The following is an example taken from the Python Reference Manual.
+
+.. productionlist::
+   try_stmt: try1_stmt | try2_stmt
+   try1_stmt: "try" ":" `suite`
+            : ("except" [`expression` ["," `target`]] ":" `suite`)+
+            : ["else" ":" `suite`]
+            : ["finally" ":" `suite`]
+   try2_stmt: "try" ":" `suite`
+            : "finally" ":" `suite`
 
 This is plain text.
 
--- get-config.pl
+++ get-config.pl
@@ -3,8 +3,13 @@ use strict;
 use warnings;
 use v5.30;
 
+use Readonly;
+
 # Default configuration filename
 my $configuration_file = "sample.conf";
+
+# Do not allow integers to start with a zero.
+Readonly my $INTEGER => qr{\A(?<Integer>(?![0])[0-9]+)\z}xms;
 
 # TODO: Parse command-line options
 #
@@ -21,18 +26,48 @@ my $configuration_file = "sample.conf";
 #   --files=file1,file2,file3,file4
 #
 for my $arg (@ARGV) {
-    # TODO: Process command-line arguments
-    if ($arg =~ m/^(-(?<ShortOption>(?<ShortOptionName>[A-Za-z]{1})[ ]?(?<ShortOptionValue>[A-Za-z0-9_-]+))|(?<LongOption>-{1}(?<LongOptionName>[a-z][a-z-]{3,})))|(?<EnvironmentVariable>(?<EnvironmentVariableName>[A-Za-z0-9_]+)(=(?<EnvironmentVariableValue>[A-Za-z0-9_ "=-]+))?)/g) {
-        if (defined($+{ShortOption})) {
-            say "Short option: $+{ShortOption}";
-        } elsif (defined($+{LongOption})) {
-            say "Long option: $+{LongOption}";
-        }
+    # TODO: Define command-line option/argument regular expression
+    Readonly my $CMDLINEOPT => qr{
+        (?<Key>[A-Za-z0-9_-]+)
+        (
+            [=]{1}
+            (?<Value>[\S]+)
+        )?
+    }xms;
+
+    if ($arg =~ m{($INTEGER)}g) {
+        say "Integer: $+{Integer}";
     }
+
+    # my @matches = {};
+    #
+    # if (@matches = $arg =~ m{$CMDLINEOPT}g) {
+    #     # if ($+{Key}) {
+    #     if (@matches != 0) {
+    #         # say "$+{Key}=$+{Value}";
+    #         say "Key=$+{Key}";
+    #     }
+    # }
+
+    # TODO: Process command-line arguments
+    # if ($arg =~ m/^(-(?<ShortOption>(?<ShortOptionName>[A-Za-z]{1})[ ]?(?<ShortOptionValue>[A-Za-z0-9_-]+))|(?<LongOption>-{1}(?<LongOptionName>[a-z][a-z-]{3,})))|(?<EnvironmentVariable>(?<EnvironmentVariableName>[A-Za-z0-9_]+)(=(?<EnvironmentVariableValue>[A-Za-z0-9_ "=-]+))?)/gmsx) {
+    #     if (defined $+{Key}) {
+    #         say "$+{Key}=$+{Value}";
+    #     }
+
+    #     # TODO: Implement regex for real
+    #     # if (defined($+{ShortOption})) {
+    #     #     say "Short option: $+{ShortOption}";
+    #     # } elsif (defined($+{LongOption})) {
+    #     #     say "Long option: $+{LongOption}";
+    #     # }
+    # }
 }
 
 # Open the configuration file
 open CONFIG_FILE, '<:encoding(UTF-8)', $configuration_file or die("Failed to open file.");
+
+say "\n\n\n\n";
 
 # TODO: Figure out how strings are initialized properly
 my $current_line = "";
